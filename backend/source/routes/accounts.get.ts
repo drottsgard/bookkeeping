a8d205164c39a0463e58538aca128081bb5864f3
Alexander Drottsgård <alexander@drottsgard.com>
2025-06-10 16:23:48 +0200

    GET /accounts builds the hierarchy

--- backend/source/routes/accounts.get.ts
+++ backend/source/routes/accounts.get.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
 import { getAllAccounts } from "../repositories/accountRepository/getAllAccounts";
+import { Account } from "../domains/account";
 
 export function getAccounts(_: Request, res: Response) {
   try {
     const accounts = getAllAccounts();
-    res.status(200).json(accounts);
+    const tree = buildTreeRecursive(accounts);
+    res.status(200).json(tree);
   } catch (exception) {
     console.error("Error fetching accounts", exception);
     res.status(500).send("Internal Server Error");
   }
+}
+
+// TODO: Move
+interface AccountTreeNode extends Account {
+  children: AccountTreeNode[];
+}
+
+function buildTreeRecursive(
+  accounts: Account[],
+  parentId: number | null = null,
+): AccountTreeNode[] {
+  return accounts
+    .filter((acc) => acc.parentId === parentId)
+    .map((acc) => ({
+      ...acc,
+      children: buildTreeRecursive(accounts, acc.id),
+    }));
 }
